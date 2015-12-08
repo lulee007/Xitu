5701c674fca86144379102ccb8b5b859d6d3fb58
lulee007 <lulee007@live.com>
2015-12-08 22:58:20 +0800

    move application to base & change launch icon

---
+++ app/src/main/java/com/lulee007/xitu/Base/XTApplication.java
@@ -0,0 +1,27 @@
+package com.lulee007.xitu.base;
+
+import android.app.Application;
+
+import com.orhanobut.logger.Logger;
+import com.squareup.leakcanary.LeakCanary;
+
+/**
+ * User: lulee007@live.com
+ * Date: 2015-12-07
+ * Time: 17:03
+ */
+public class XTApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        //pretty logger
+        Logger.init("XTAppLog");
+
+        //内存泄露检测
+        LeakCanary.install(this);
+
+
+
+    }
+}
